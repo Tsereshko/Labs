bab35872ade9c701052f842755cb8794f59ea575
Rodion <rio228pk@gmail.com>
2022-11-24 23:11:32 +0300

    commit2

--- DatabaseSQL/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.databasesql {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.databasesql to javafx.fxml;
-    exports com.example.databasesql;
-}
